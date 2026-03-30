d16a20185e91a37814ab398743da55997184db6e
abcdefg202210-arch <abcdefg202210@gmail.com>
2026-03-31 02:18:46 +0800

    Create token1.sol

---
+++ token1.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+//基础标准 ERC20 代币（最简发行版）
+
+// 导入官方标准ERC20合约
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+// 最简标准ERC20代币
+contract BasicERC20 is ERC20 {
+    // 构造函数：代币名称、代币符号
+    constructor() ERC20("MyBasicToken", "MBT") {
+        // 一次性铸造 1亿 代币给部署者（18位小数，标准配置）
+        _mint(msg.sender, 100_000_000 * 10 ** decimals());
+    }
+}
